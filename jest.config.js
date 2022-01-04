a421e806d7fb7bf190843ca41e27e27678ea746d
oscario2 <oscarandersson550@gmail.com>
2022-01-04 23:36:08 +0100

    chore: update `.env` and add js transformer to `jest`

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jsdom', // 'node'
   globals: {
     'ts-jest': {
@@ -10,9 +13,9 @@ module.exports = {
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx)$': '',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
   transformIgnorePatterns: ['node_modules', 'lib'],
   modulePaths: ['<rootDir>'], // baseUrl in tsconfig.json
-  collectCoverage: true,
+  collectCoverage: process.env.COVERAGE === '1',
 };
